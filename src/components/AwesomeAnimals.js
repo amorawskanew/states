5a693c1da903eb85f62c6db37d8330a79481364d
amorawskanew <amorawska@yahoo.com>
2020-02-11 11:30:43 +0100

    commit

---
+++ src/components/AwesomeAnimals.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default class AwesomeAnimals extends React.Component {
+  state = {
+    animalsByAwesomeness: [
+      "Chicken",
+      "Sloth",
+      "Porcupine",
+      "Killer whale",
+      "Velociraptor"
+    ]
+  };
+  render() {
+    return <ul>{/* TODO */}</ul>;
+  }
+}
